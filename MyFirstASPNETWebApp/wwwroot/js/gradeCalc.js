ad1620d50283fb01168a2b0db47397f35eb5a917
benjark <90466204+benjark@users.noreply.github.com>
2022-01-11 22:06:14 -0700

    third

--- MyFirstASPNETWebApp/wwwroot/js/gradeCalc.js
+++ MyFirstASPNETWebApp/wwwroot/js/gradeCalc.js
@@ -3,14 +3,14 @@
 $("#BtnSend").click(function () {
 
     //Turning the input into integers which can be used by the function.
-    
-    var QZ = parseInt(document.getElementById("QZ").value);
-    var EX = parseInt(document.getElementById("EX").value);
-    var IN = parseInt(document.getElementById("IN").value);
-    var GP = parseInt(document.getElementById("GP").value);
-    var AS = parseInt(document.getElementById("AS").value);
 
-    //Calculating the final grade.
+    var QZ = parseInt($("#QZ").val(), 10);
+    var EX = parseInt($("#EX").val(), 10);
+    var IN = parseInt($("#IN").val(), 10);
+    var GP = parseInt($("#GP").val(), 10);
+    var AS = parseInt($("#AS").val(), 10);
+
+    //Calculating the final grade and letter grade.
 
     finalGrade = ((QZ * .10)
         + (EX * .20)
@@ -18,12 +18,38 @@ $("#BtnSend").click(function () {
         + (GP * .05)
         + (AS * .55))
 
+    if (finalGrade >= 94)
+        letterGrade = "A";
+    else if (finalGrade >= 90)
+        letterGrade = "A-";
+    else if (finalGrade >= 87)
+        letterGrade = "B+";
+    else if (finalGrade >= 84)
+        letterGrade = "B";
+    else if (finalGrade >= 80)
+        letterGrade = "B-";
+    else if (finalGrade >= 77)
+        letterGrade = "C+";
+    else if (finalGrade >= 74)
+        letterGrade = "C";
+    else if (finalGrade >= 70)
+        letterGrade = "C-";
+    else if (finalGrade >= 67)
+        letterGrade = "D+";
+    else if (finalGrade >= 64)
+        letterGrade = "D";
+    else if (finalGrade >= 60)
+        letterGrade = "D-";
+    else
+        letterGrade = "F";
+
 //Displaying alert with the final grade.
 
-    alert("Your final grade is " + finalGrade + "%." );
+    alert("Your final grade is " + finalGrade + "%." + " This means your letter grade is a(n) " + letterGrade + "." );
 
 
 //Animation which removes the picture upon the pressing of the button. 
+
     $("#picme").fadeOut("slow");
 
 })
